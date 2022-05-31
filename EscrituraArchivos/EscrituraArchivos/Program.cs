ed8486c95ac2d4de25675ad48af654db212b59c4
IsaacFlaminHot <l21212024@tectijuana.edu.mx>
2022-05-31 11:25:54 -0700

    EscrituraArchivos

--- EscrituraArchivos/EscrituraArchivos/Program.cs
+++ EscrituraArchivos/EscrituraArchivos/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace EscrituraArchivos
 {
@@ -10,6 +11,25 @@ namespace EscrituraArchivos
     {
         static void Main(string[] args)
         {
+            StreamWriter sw = new StreamWriter("ejemplo.txt");
+            //si el archo no existe lo creará
+            //si ya existe, escribira en él
+
+            string[] lines =
+            {
+                "Esta es la informacón de la primea línea",
+                "Esta es la segunda línea",
+                "Fin del texto"
+            };
+
+            foreach (string line in lines)
+            {
+                sw.WriteLine(line);
+            }
+            sw.Close(); // se debe cerrar el archivo
+
+            Console.WriteLine("Escribiendo en el archivo..");
+            Console.ReadLine();
         }
     }
 }
