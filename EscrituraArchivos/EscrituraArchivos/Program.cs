f97bdb0f19c58eddf5af50eca783730b0f47a94a
IsaacFlaminHot <l21212024@tectijuana.edu.mx>
2022-06-02 00:24:34 -0700

    Actualización de Tarea

--- EscrituraArchivos/EscrituraArchivos/Program.cs
+++ EscrituraArchivos/EscrituraArchivos/Program.cs
@@ -9,27 +9,63 @@ namespace EscrituraArchivos
 {
     class Program
     {
+        class Alumno
+        {
+            public string nombre;
+            public int edad;
+            public string genero;
+            public string carrera;
+
+            public Alumno(string nombre, int edad, string genero, string carrera)
+            {
+                this.nombre = nombre;
+                this.edad = edad;
+                this.genero = genero;
+                this.carrera = carrera;
+            }
+
+            public void DesplegarDatos()
+            {
+                Console.WriteLine("El nombre del alumno es: {0}", nombre);
+                Console.WriteLine("La edad del alumno es: {0}",edad);
+                Console.WriteLine("El genero del alumno es de tipo: {0}",genero);
+                Console.WriteLine("La carrera a estudiar es: {0}",carrera);
+            }
+        }
         static void Main(string[] args)
         {
-            StreamWriter sw = new StreamWriter("ejemplo.txt", true);
+            StreamWriter sw = new StreamWriter("DocumentoAlumnos.txt", true);
             //si el archo no existe lo creará
             //si ya existe, escribira en él
 
-            string[] lines = new string[3];
+            //declaracion de varaibles:
+            string nombre, genero, carrera;
+            int edad;
 
-            Console.WriteLine("¿Cuál es el nombre del usuario?: ");
-            lines[0] = Console.ReadLine();
-            Console.WriteLine("¿Cual es la edad?: ");
-            lines[1] = Console.ReadLine();
-            Console.WriteLine("¿Cual su genero?: ");
+            //captura de variables:
+            Console.Write("¿Cuál es el nombre del alumno?: ");
+            nombre = Console.ReadLine();
+            Console.Write("¿Cual es la edad del alumno?: ");
+            edad = int.Parse(Console.ReadLine());
+            Console.Write("¿Cual su genero?: ");
+            genero = Console.ReadLine();
+            Console.Write("¿Cuál es la carrera que está estudiando el alumno?: ");
+            carrera = Console.ReadLine();
+            Console.Clear();
+            //Creacion del Objeto de la Clase Alumno
+            Alumno A1 = new Alumno(nombre, edad, genero, carrera);
 
-            lines[2] = Console.ReadLine(); 
+            A1.DesplegarDatos();
 
-            //recorrer el arreglo 
-            foreach (string line in lines)
-            {
-                sw.WriteLine(line);//escribe en el archivo 
-            }
+            Console.WriteLine("Presione Enter para finalizar. ");
+            Console.ReadKey();
+            Console.Clear();
+
+            sw.WriteLine("El nombre del alumno es: {0}", A1.nombre);//escribe en el archivo 
+            sw.WriteLine("La edad del alumno es: {0}", A1.edad);
+            sw.WriteLine("El genero del alumno es de tipo: {0}", A1.genero);
+            sw.WriteLine("La carrera a estudiar es: {0}", A1.carrera);
+
             sw.Close(); // se debe cerrar el archivo
 
             Console.WriteLine("Escribiendo en el archivo..");
